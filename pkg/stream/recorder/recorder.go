69e85379ec8ff64b5ae0a43a6decf36276263795
Antony Ho <ntonyworkshop@gmail.com>
2020-02-12 10:57:21 +0800

    Simply record implementation

--- pkg/stream/recorder/recorder.go
+++ pkg/stream/recorder/recorder.go
@@ -1,7 +1,9 @@
 package downloader
 
 import (
+	"errors"
 	"github.com/antonyho/crhk-recorder/pkg/stream/url"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/antonyho/crhk-recorder/pkg/stream/resolver"
@@ -9,11 +11,23 @@ import (
 
 // Record the given channel
 func Record(channel string) {
-	playlist, err := resolver.Find(channel)
+	channelName, playlist, err := resolver.Find(channel)
 	if err != nil {
 		panic(err)
 	}
 	for _, track := range playlist {
-		resp, err := http.Get(url.StreamMediaURL("881hd", track.Path))
+		resp, err := http.Get(url.StreamMediaURL(channelName, track.Path))
+		if err != nil {
+			panic(err)
+		}
+		media, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			panic(err)
+		}
+
+		if len(media) == 0 {
+			panic(errors.New("Empty media file"))
+		}
 	}
 }
